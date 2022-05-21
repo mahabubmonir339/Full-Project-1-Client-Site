5676752ad318f9ce5b26d3ce012025504a756665
mahabubmonir339 <mahabubhossenmonir@gmail.com>
2022-05-21 11:03:31 +0600

    addd

--- src/Page-1/Home/Services.js
+++ src/Page-1/Home/Services.js
@@ -5,7 +5,7 @@ import ServicesCardB from './ServicesCardB';
 const Services = () => {
     return (
         <div className='mt-20'>
-            <h1 className='text-center text-[#d946ef]'>Our Services</h1><br />
+            <h1 className='text-center text-[#d946ef]'>Our Services</h1>
             <h3 className='text-center text-2xl'><b>Services We Provide</b></h3><br /><br />
             <ServicesCardA></ServicesCardA>
             <ServicesCardB></ServicesCardB>
--- src/Page-1/Home/ServicesCardB.js
+++ src/Page-1/Home/ServicesCardB.js
@@ -7,8 +7,8 @@ const ServicesCardB = () => {
             <div class="hero min-h-screen">
                 <div class="hero-content flex justify-evenly flex-col lg:flex-row px-10">
                     <div><img src={Treatment} class="max-w-sm rounded-lg shadow-2xl" /></div>
-                    <div class="text-start">
-                        <h1 class="text-5xl font-bold">Box Office News!</h1>
+                    <div class="text-start mx-20 px-10">
+                        <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
                         <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         <button class="btn btn-primary">Get Started</button>
                     </div>
